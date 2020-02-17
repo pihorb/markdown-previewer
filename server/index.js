9b632a075a8ef603db5ffbd8825d0962d01cbc7b
Ihor666777 <ihorpylypyak@gmail.com>
2020-02-17 20:33:20 +0200

    initial

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from cors
+
+const app = express()
+
+app.use(cors())
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
